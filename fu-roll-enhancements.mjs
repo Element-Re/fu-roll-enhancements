904e455a8740043ba55d50f7c99177dd4a039f2d
Element_Re <elementalajah@gmail.com>
2025-10-02 12:26:11 -0700

    chore: code cleanup

--- fu-roll-enhancements.mjs
+++ fu-roll-enhancements.mjs
@@ -1,20 +1,20 @@
-import { rollEnhancements } from './module/rolls.mjs';
-import { registerKeyBindings, registerSettings } from './module/settings.mjs';
-import { initializeTemplates } from './module/templates.mjs';
-import { registerAutoTargetHooks } from './module/autoTarget.mjs';
-import { initializeActiveEffects } from './module/effects.js';
+import {rollEnhancements} from './module/rolls.mjs';
+import {registerKeyBindings, registerSettings} from './module/settings.mjs';
+import {initializeTemplates} from './module/templates.mjs';
+import {registerAutoTargetHooks} from './module/autoTarget.mjs';
+import {initializeActiveEffects} from './module/effects.js';
 import {getHeaderControlsItemSheetV2} from './module/documents/RollConfiguration.js';
 
 Hooks.once('init', () => {
-	registerSettings();
-	registerKeyBindings();
-  	initializeTemplates();
-	initializeActiveEffects();
-	registerAutoTargetHooks();
+    registerSettings();
+    registerKeyBindings();
+    initializeTemplates();
+    initializeActiveEffects();
+    registerAutoTargetHooks();
 });
 
 Hooks.once('ready', () => {
-	libWrapper.register('fu-roll-enhancements', 'CONFIG.Item.documentClass.prototype.roll', rollEnhancements);
+    libWrapper.register('fu-roll-enhancements', 'CONFIG.Item.documentClass.prototype.roll', rollEnhancements);
 });
 
 Hooks.on('getHeaderControlsItemSheetV2', getHeaderControlsItemSheetV2);
