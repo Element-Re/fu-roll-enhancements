e5d1261b6b10c5a41fcde5ed32e617a3b8916ebb
Element_Re <elementalajah@gmail.com>
2026-05-15 14:42:16 -0700

    chore: auto target results polish

--- fu-roll-enhancements.mjs
+++ fu-roll-enhancements.mjs
@@ -2,7 +2,7 @@ import {rollEnhancements} from './module/rolls.mjs';
 import {registerKeyBindings, registerSettings} from './module/settings.mjs';
 import {initializeTemplates} from './module/templates.mjs';
 import {registerAutoTargetHooks} from './module/autoTarget/autoTarget.mjs';
-import {initializeActiveEffects} from './module/effects.js';
+import {initializeActiveEffects} from './module/effects.mjs';
 import {getHeaderControlsItemSheetV2} from './module/documents/RollConfiguration.js';
 
 Hooks.once('init', () => {
