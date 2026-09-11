c754628bbb2ec64ec3e59fc5cdecd31694d98770
Element_Re <elementalajah@gmail.com>
2026-09-11 05:14:19 -0700

    chore: clarify comment

--- module/helpers/effects.mjs
+++ module/helpers/effects.mjs
@@ -1,7 +1,7 @@
 import {MODULE} from './utils.mjs';
 
 /**
- * Gets the numerical weighted chance of a target based on an actor flag, if present.
+ * Gets the numerical weighted chance of a token being targeted based on an actor flag, if present.
  */
 export function getTargetWeight(token) {
 
